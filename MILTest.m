661717662f49e3ea83ffa6bf02454144d729a962
David Hall <davidndc@Davids-Mac-mini-2.local>
2013-10-16 16:35:56 -0400

    added a test class

---
+++ MILTest.m
@@ -0,0 +1,13 @@
+//
+//  MILTest.m
+//  GITTest
+//
+//  Created by David Hall on 10/16/13.
+//  Copyright (c) 2013 David Hall. All rights reserved.
+//
+
+#import "MILTest.h"
+
+@implementation MILTest
+
+@end
